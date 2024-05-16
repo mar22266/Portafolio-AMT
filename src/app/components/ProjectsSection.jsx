9e3ede720f5984ff4e1383c2e68cc68298fd2087
Andre Marroquin <mar22266@uvg.edu.gt>
2024-05-16 08:49:08 -0600

    Feat: Project sections addedimgs also

---
+++ src/app/components/ProjectsSection.jsx
@@ -0,0 +1,113 @@
+"use client";
+import React, { useState } from 'react';
+import ProjectCard from './ProjectCard';
+import ProjectTag from './ProjectTag';
+
+const projectsData = [
+    {
+        id: 1,
+        title: "React Portfolio Website",
+        description: "Project 1 description",
+        image: "/images/projects/portafolio.png",
+        tag: ["All", "Web"],
+        gitUrl: "/",
+        previewUrl: "/",
+    },
+    {
+        id: 2,
+        title: "Android Application Nutrition App",
+        description: "Project 2 description",
+        image: "/images/projects/android.png",
+        tag: ["All", "Mobile"],
+        gitUrl: "/",
+        previewUrl: "/",
+    },
+    {
+        id: 3,
+        title: "Soccer Blog Website",
+        description: "Project 3 description",
+        image: "/images/projects/blog.png",
+        tag: ["All", "Web"],
+        gitUrl: "/",
+        previewUrl: "/",
+    },
+    {
+        id: 4,
+        title: "Calculator Web App",
+        description: "Project 4 description",
+        image: "/images/projects/calculator.png",
+        tag: ["All", "Web"],
+        gitUrl: "/",
+        previewUrl: "/",
+    },
+    {
+        id: 5,
+        title: "Determinant Matrix Calculator",
+        description: "Project 5 description",
+        image: "/images/projects/matrix.png",
+        tag: ["All", "Web"],
+        gitUrl: "/",
+        previewUrl: "/",
+    },
+    {
+        id: 6,
+        title: "Lisp Interpreter in Java",
+        description: "Project 6 description",
+        image: "/images/projects/lisp.png",
+        tag: ["All", "Web"],
+        gitUrl: "/",
+        previewUrl: "/",
+    },
+];
+
+const ProjectsSection = () => {
+    const [tag, setTag] = useState('All');
+
+    const handleTagChange = (newTag) => {
+        setTag(newTag);
+    };
+
+    const filteredProjects = projectsData.filter((project) => {
+        return project.tag.includes(tag);
+    });
+
+    return (
+        <>
+            <h2 className='text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12'>My Projects</h2>
+            <div className='text-white flex flex-row justify-center items-center gap-2 py-6'>
+                <ProjectTag
+                    onClick={handleTagChange}
+                    name='All'
+                    isSelected={tag === 'All'}
+                />
+                <ProjectTag
+                    onClick={handleTagChange}
+                    name='Web'
+                    isSelected={tag === 'Web'}
+                />
+                <ProjectTag
+                    onClick={handleTagChange}
+                    name='Mobile'
+                    isSelected={tag === 'Mobile'}
+                />
+            </div>
+            <div className="px-4 md:px-8 lg:px-16">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+                    {filteredProjects.map((project) => (
+                        <div className="w-full" key={project.id}>
+                            <ProjectCard
+                                title={project.title}
+                                description={project.description}
+                                imgUrl={project.image}
+                                gitUrl={project.gitUrl}
+                                previewUrl={project.previewUrl}
+                            />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default ProjectsSection;
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Section from "./components/Section";
 import NavBar from "./components/NavBar";
 import AboutSection from "./components/AboutSection";
+import ProjectsSection from "./components/ProjectsSection";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <div class='container mt-24 mx-auto px-12 py-4'>
         <Section />
         <AboutSection />
+        <ProjectsSection />
       </div>
     </main>
   );
