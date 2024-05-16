f759c923b13e8865c9b11d7e4e62d51784a0c68f
Andre Marroquin <mar22266@uvg.edu.gt>
2024-05-15 20:57:19 -0600

    Feat: Menu overlay added

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Section from "./components/Section";
+import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212] ">
-      <div class='container mx-auto px-12 py-4'>
+      <NavBar />
+      <div class='container mt-24 mx-auto px-12 py-4'>
         <Section />
       </div>
     </main>
