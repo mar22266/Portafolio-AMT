ae2f9dc669bbe9f80b731ccd98c3ff47b280568c
Andre Marroquin <mar22266@uvg.edu.gt>
2024-05-16 15:22:58 -0600

    Fix: achievements added

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
+import AchievementsSection from "./components/AchievementsSection";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <NavBar />
       <div class='container mt-24 mx-auto px-12 py-4'>
         <Section />
+        <AchievementsSection />
         <AboutSection />
         <ProjectsSection />
         <EmailSection />
