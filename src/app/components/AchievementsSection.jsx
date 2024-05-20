186cb7c5b5616099a7398a3f51fc4d435d338e08
Andre Marroquin <mar22266@uvg.edu.gt>
2024-05-20 09:29:14 -0600

    Fix: achievement section fixed

--- src/app/components/AchievementsSection.jsx
+++ src/app/components/AchievementsSection.jsx
@@ -27,29 +27,28 @@ const achievementsList = [
 
 const AchievementsSection = () => {
     return (
-        <div className='py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
-            <div className='border-[#33353F] border rounded-md py-8 px-16 flex flex-row items-center justify-between '>
+        <div className='py-8 px-4 sm:py-16 xl:gap-16 xl:px-16'>
+            <div className='border-[#33353F] border rounded-md py-8 px-4 sm:px-8 md:px-12 lg:px-16 flex flex-col sm:flex-row sm:items-center sm:justify-between'>
                 {achievementsList.map((achievement, index) => {
                     return (
-                        <div key={index} className='flex flex-col items-center justify-center mx-4'>
-                            <h2 className='text-white text-4xl font-bold flex flex-row'>
+                        <div key={index} className='flex flex-col items-center justify-center mx-2 sm:mx-4 my-4 sm:my-0'>
+                            <h2 className='text-white text-2xl sm:text-3xl md:text-4xl font-bold flex flex-row'>
                                 <AnimatedNumbers
                                     includeComma
                                     animateToNumber={parseInt(achievement.value)}
                                     locale='en-US'
-                                    className='text-white text-4xl font-bold'
+                                    className='text-white text-2xl sm:text-3xl md:text-4xl font-bold'
                                     configs={((_, index) => {
                                         return {
                                             mass: 2,
                                             friction: 50,
                                             tension: 70 * (index + 1),
                                         }
-                                    }
-                                    )}
+                                    })}
                                 />
                                 {achievement.postfix}
                             </h2>
-                            <p className='text-[#ADB7BE] text-base'>
+                            <p className='text-[#ADB7BE] text-sm sm:text-base'>
                                 {achievement.metric}
                             </p>
                         </div>
@@ -59,5 +58,6 @@ const AchievementsSection = () => {
         </div>
     )
 }
+
 
 export default AchievementsSection
